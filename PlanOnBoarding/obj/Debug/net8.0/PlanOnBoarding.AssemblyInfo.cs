4dc660fd88eed0fd15ef18741688551f6665d3d7
Aswin <aswin.k@cspl.com>
2026-05-07 16:44:20 +0530

    adding packages

--- PlanOnBoarding/obj/Debug/net8.0/PlanOnBoarding.AssemblyInfo.cs
+++ PlanOnBoarding/obj/Debug/net8.0/PlanOnBoarding.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlanOnBoarding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4bb0370c696d447f85615bda202cce04f010386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ec845aa6503d42a6b8d12f97df9033e4d18e91")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlanOnBoarding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlanOnBoarding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
