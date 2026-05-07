23cf83a6a5811d4a4c43445c06414bfbfb2d58fe
Aswin <aswin.k@cspl.com>
2026-05-07 17:51:59 +0530

    update file name

--- Helpers/obj/Debug/net8.0/Helpers.AssemblyInfo.cs
+++ Helpers/obj/Debug/net8.0/Helpers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ec845aa6503d42a6b8d12f97df9033e4d18e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc660fd88eed0fd15ef18741688551f6665d3d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
